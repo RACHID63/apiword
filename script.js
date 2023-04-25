cc9ebf4c8787187aa99d00c27bcfa19f15861b00
LAIEB Rachid <rachid.laieb@gmail.com>
2023-04-25 12:03:22 +0200

    catch word meaning and audio

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// let wordToSearch = "";
+let wordToSearch = "";
 
 const watchSubmit = () => {
   const form = document.querySelector("#form");
@@ -6,19 +6,52 @@ const watchSubmit = () => {
     event.preventDefault();
     const data = new FormData(form);
     const search = data.get("search");
-    const wordToSearch = data.get("search");
+    wordToSearch = search;
     apiCall(wordToSearch);
   });
 };
 
-// envoyer mon mot à l'api
 const apiCall = (word) => {
-  console.log("wrd to search", word);
-  fetch('https://api.dictionaryapi.dev/api/v2/entries/en/${word}')
-    .then((Response) => Response.json())
+  fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("not found");
+      }
+      return response.json();
+    })
     .then((data) => {
-      console.log(data);
+      const wordInformations = data[0];
+      const word = wordInformations.word;
+      const phonetics = findProp(wordInformations.phonetics, "text");
+      console.log("word", phonetics);
+      // prononciation audio
+      const pronon = findProp(wordInformations.phonetics, "audio");
+      console.log("pronom", pronon);
+      const meanings = wordInformations.meanings
+      console.log("meanings", meanings);
+      
+
+    })
+    .catch((error) => {
+      if (error.message === "not found") {
+        alert("The word does not exist.");
+      } else {
+        console.error(error);
+      }
     });
+};
+
+const findProp = (array, name) => {
+  for (let i = 0; i < array.length; i++) {
+    const currentObject = array[i];
+    const hasProp = currentObject.hasOwnProperty(name)
+    if (hasProp) {
+      // console.log("has :" , hasProp, i )
+      return currentObject[name];
+    }
+    
+  }
+  return null;
 };
 
 watchSubmit();
